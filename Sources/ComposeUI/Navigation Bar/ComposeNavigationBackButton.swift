8fffd1ff1d28f1327350bdd5dad4dab5e3b508f6
Daniel Vershinin <d@sgml.me>
2022-04-13 17:56:01 +0300

    Easier to tap back button.

--- Sources/ComposeUI/Navigation Bar/ComposeNavigationBackButton.swift
+++ Sources/ComposeUI/Navigation Bar/ComposeNavigationBackButton.swift
@@ -27,13 +27,31 @@ public struct ComposeNavigationBackButton : View {
                     .resizable()
                     .aspectRatio(contentMode: .fit)
                     .frame(width: 17, height: 17)
+                    .padding(.leading, -8)
+                    .shadow(color: Color.black.opacity(0.5), radius: 5, x: 0, y: 0)
             }
             .frame(width: 25, alignment: .leading)
+            .padding(8)
             .contentShape(Rectangle())
         }
         .buttonStyle(PlainButtonStyle())
     }
     
+}
+
+struct ComposeNavigationBackButton_Previews: PreviewProvider {
+    
+    static var previews: some View {
+        Group {
+            VStack {
+                Text("Navigation styles")
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .composeNavigationBar(title: "Example", backButtonEmitter: SignalEmitter())
+            .composeNavigationBarStyle(.init(foregroundColor: .white))
+        }
+        .preferredColorScheme(.dark)
+    }
 }
 
 #endif
