405ea8e41d50a4617c6777c831e691907d9c8cd2
Daniel Vershinin <d@sgml.me>
2022-04-26 16:24:54 +0300

    Component id fixes.

--- Sources/Compose/Component/Component+Id.swift
+++ Sources/Compose/Component/Component+Id.swift
@@ -1,9 +1,10 @@
 import Foundation
+import SwiftUI
 
 extension Storage {
 
     struct ComponentIdentifierKey : Hashable {
-        let id : String
+        let id : Int
         let keyPath = \Component.self
     }
     
@@ -12,14 +13,24 @@ extension Storage {
 extension Component {
     
     public var id : UUID {
-        if let id = Storage.shared.value(at: Storage.ComponentIdentifierKey(id: String(describing: self))) as? UUID {
+        let internalId = String(describing: self).hashValue
+        
+        if let id = Storage.shared.value(at: Storage.ComponentIdentifierKey(id: internalId)) as? UUID {
             return id
         }
         else {
             let id = UUID()
-            Storage.shared.setValue(id, at: Storage.ComponentIdentifierKey(id: String(describing: self)))
+            Storage.shared.setValue(id, at: Storage.ComponentIdentifierKey(id: internalId))
             return id
         }
+    }
+    
+}
+
+extension SwiftUI.ObservedObject : CustomDebugStringConvertible {
+    
+    public var debugDescription : String {
+        "ObservedObject()"
     }
     
 }
