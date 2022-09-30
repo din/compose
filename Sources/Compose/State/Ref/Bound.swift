6d95d9d674da6c3bcd8fcc89396b616e7fce1e44
Daniel Vershinin <d@sgml.me>
2022-09-30 08:14:51 +0300

    Bound property wrapper.

--- Sources/Compose/State/Ref/Bound.swift
+++ Sources/Compose/State/Ref/Bound.swift
@@ -1,8 +1,35 @@
-//
-//  File.swift
-//  
-//
-//  Created by Daniel Vershinin on 30.09.2022.
-//
-
 import Foundation
+import SwiftUI
+
+@propertyWrapper public struct Bound<T : Equatable> : Equatable {
+    
+    public static func == (lhs: Bound<T>, rhs: Bound<T>) -> Bool {
+        return lhs.wrappedValue == rhs.wrappedValue
+    }
+    
+    public var wrappedValue : T {
+        
+        didSet {
+            self.binding.wrappedValue = wrappedValue
+        }
+        
+    }
+    
+    public var projectedValue : Binding<T> {
+        get {
+            self.binding
+        }
+        set {
+            self.binding = newValue
+            self.wrappedValue = newValue.wrappedValue
+        }
+    }
+    
+    fileprivate var binding : Binding<T>
+    
+    public init(wrappedValue : T) {
+        self.wrappedValue = wrappedValue
+        self.binding = .constant(wrappedValue)
+    }
+    
+}
